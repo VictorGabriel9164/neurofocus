72a23c859e7736032866f92afd84cf775cd1d7f4
VictorGabriel9164 <victorgabriel67.nr@gmail.com>
2026-04-20 01:35:21 -0300

    feat: add color in style.css and add div to redirect for add task page

--- src/routes/page/home.tsx
+++ src/routes/page/home.tsx
@@ -1,12 +1,21 @@
-import Navbar from "#/components/neuroNavbar";
 import { createFileRoute } from "@tanstack/react-router";
-
+import { Button } from "#/components/ui/button";
+import Navbar from "#/components/neuroNavbar";
 export const Route = createFileRoute("/page/home")({ component: App });
 
 function App() {
   return (
     <>
-      <Navbar />
+      <header>
+        <Navbar />
+      </header>
+      <main className="flex justify-center items-center font-rubik">
+        <div className="bg-neuro-light-blue mt-8 w-[1300px] h-[300px] rounded-2xl  flex items-start justify-start flex-col gap-3">
+          <h1 className="m-8 mb-0 text-5xl font-semibold">Olá!</h1>
+          <h1 className="m-8 mt-0 text-2xl">Suas tarefas estão aqui:</h1>
+          <Button variant={"outline"} className=" m-12 mr-18 bg-neuro-light-green hover:bg-neuro-dark-green border-none px-14 rounded-3xl text-white">Adicionar Tarefa</Button>
+        </div>
+      </main>
     </>
   );
 }
