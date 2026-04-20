081f4b3971caf2d8df77526367a7fdf87120c780
VictorGabriel9164 <victorgabriel67.nr@gmail.com>
2026-04-19 22:25:18 -0300

    feat: add components to login page fix: personalized colors

--- src/routes/page/login.tsx
+++ src/routes/page/login.tsx
@@ -1,12 +1,32 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, Outlet } from "@tanstack/react-router";
+import { Input } from "#/components/ui/input";
+import { Field, FieldLabel } from "@/components/ui/field";
+import { Button } from "#/components/ui/button";
+import { Link } from '@tanstack/react-router'
 
-export const Route = createFileRoute('/page/login')({
+export const Route = createFileRoute("/page/login")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
   return (
     <>
+      <main className="flex flex-col items-center justify-center min-h-screen">
+        <h1 className="text-4xl font-bold mb-8">Login</h1>
+        <div className="flex flex-col gap-8">
+          <Field className="gap-1 mt-3">
+          <FieldLabel className="font-bold">E-mail</FieldLabel>
+          <Input className="rounded-3xl border-neuro-light-green focus-visible:border-neuro-dark-green focus-visible:ring-neuro-dark-green mr-32"></Input>
+          </Field>
+
+          <Field className="gap-1 mt-3">
+            <FieldLabel className="font-bold">Senha</FieldLabel>
+          <Input className="rounded-3xl border-neuro-light-green focus-visible:border-neuro-dark-green focus-visible:ring-neuro-dark-green mr-32"></Input>
+          </Field>
+        </div>
+        <Button variant={"outline"} className="mt-14 bg-neuro-dark-green border-none px-12 rounded-3xl">Entrar</Button>
+        <h1>Não tem uma conta?</h1>
+      </main>
     </>
-  )
+  );
 }
