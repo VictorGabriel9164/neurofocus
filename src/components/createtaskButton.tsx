5acbac97e799d0815037165a06339bed70f7c2d4
VictorGabriel9164 <victorgabriel67.nr@gmail.com>
2026-05-19 20:50:08 -0300

    feat: add alert dialog shadcn component and add a button component for create a task fix: change the "adicionar tarefa"  color button

---
+++ src/components/createtaskButton.tsx
@@ -0,0 +1,120 @@
+import { useState } from "react";
+import { cn } from "#/lib/utils";
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+
+type CreateTaskButtonProps = {
+  className?: string;
+  title?: string;
+  subjects?: string;
+  time?: string;
+  state?: boolean;
+};
+
+const CreateTaskButton = ({ className }: CreateTaskButtonProps) => {
+  const [taskTitle, setTaskTitle] = useState("");
+  const [taskDescription, setTaskDescription] = useState("");
+  const [taskDate, setTaskDate] = useState("");
+  const [taskTime, setTaskTime] = useState("");
+
+  const handleCreateTask = () => {
+    console.log({
+      title: taskTitle,
+      description: taskDescription,
+      date: taskDate,
+      time: taskTime,
+    });
+
+    alert("Atividade criada com sucesso!");
+  };
+
+  return (
+    <div className={cn(className)}>
+      <AlertDialog>
+        <AlertDialogTrigger asChild>
+          <Button className="bg-neuro-light-green hover:bg-neuro-dark-green border-none px-14 rounded-3xl text-white">
+            Criar atividade
+          </Button>
+        </AlertDialogTrigger>
+
+        <AlertDialogContent className="sm:max-w-125 bg-gray-200 border-gray-300">
+          <AlertDialogHeader>
+            <AlertDialogTitle>Nova atividade</AlertDialogTitle>
+
+            <AlertDialogDescription>
+              Defina as informações da atividade abaixo.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+
+          <div className="flex flex-col gap-4 py-4">
+            <div className="flex flex-col gap-2">
+              <label className="text-sm font-medium">Título</label>
+
+              <Input
+                placeholder="Digite o título da atividade"
+                value={taskTitle}
+                onChange={(e) => setTaskTitle(e.target.value)}
+              />
+            </div>
+
+            <div className="flex flex-col gap-2">
+              <label className="text-sm font-medium">Descrição</label>
+
+              <Textarea
+                placeholder="Digite a descrição da atividade"
+                value={taskDescription}
+                onChange={(e) => setTaskDescription(e.target.value)}
+              />
+            </div>
+
+            <div className="grid grid-cols-2 gap-4">
+              <div className="flex flex-col gap-2">
+                <label className="text-sm font-medium">Data de entrega</label>
+
+                <Input
+                  type="date"
+                  value={taskDate}
+                  onChange={(e) => setTaskDate(e.target.value)}
+                />
+              </div>
+
+              <div className="flex flex-col gap-2">
+                <label className="text-sm font-medium">Horário</label>
+
+                <Input
+                  type="time"
+                  value={taskTime}
+                  onChange={(e) => setTaskTime(e.target.value)}
+                />
+              </div>
+            </div>
+          </div>
+
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancelar</AlertDialogCancel>
+
+            <AlertDialogAction onClick={handleCreateTask}>
+              Salvar atividade
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </div>
+  );
+};
+
+export default CreateTaskButton;
--- src/routes/page/gerenciartarefaPage.tsx
+++ src/routes/page/gerenciartarefaPage.tsx
@@ -1,5 +1,7 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { Button } from '#/components/ui/button';
+
+import TaskComponent from '#/components/createtaskButton';
 import Navbar from "#/components/neuroNavbar";
 export const Route = createFileRoute('/page/gerenciartarefaPage')({
   component: RouteComponent,
@@ -15,12 +17,9 @@ function RouteComponent() {
       <div className='bg-neuro-orange mt-8 w-325 h-70 rounded-2xl  flex items-start justify-start flex-col gap-3'>
         <h1 className="m-8 mb-0 text-5xl font-semibold">Minhas Tarefas:</h1>
         <h1 className="m-8 mt-2 text-2xl">Organize seus estudos e acompanhe suas atividades</h1>
-         <Button
-            variant={"outline"}
-            className="ml-14 mr-18 bg-neuro-light-green hover:bg-neuro-dark-green border-none px-14 rounded-3xl text-white"
-          >
-            Adicionar Tarefa
-          </Button>
+        <div className='ml-14 mr-18 px-14'>
+         <TaskComponent/>
+         </div>
       </div>
     </main>
     </>
